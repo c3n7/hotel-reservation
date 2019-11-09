fa0abbb02a97226d40a1c4e1e5ba651684885cba
Timothy Karani <kartimothy@gmail.com>
2019-11-09 08:03:22 +0300

    Fixed images in admin view

--- hotel_reservation_project/settings.py
+++ hotel_reservation_project/settings.py
@@ -139,3 +139,6 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- hotel_reservation_project/urls.py
+++ hotel_reservation_project/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path('roomdashboard/', include('dashboard.urls')),
     path('', include('rooms.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
