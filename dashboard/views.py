245d020664519592fc19b32f98ad6baa2b6e3c90
Timothy Karani <kartimothy@gmail.com>
2019-11-11 06:25:15 +0300

    Added authorization mixins to roomdashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,29 +1,39 @@
+from braces.views import (
+    LoginRequiredMixin,
+    SuperuserRequiredMixin,
+)
+
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from rooms.models import RoomCategory
 
-class RoomDashboardListView(ListView):
+class RoomDashboardListView(LoginRequiredMixin, SuperuserRequiredMixin, ListView):
     model = RoomCategory
     template_name = 'roomdashboard_list.html'
+    login_url = 'login'
 
-class RoomDashboardDetailView(DetailView):
+class RoomDashboardDetailView(LoginRequiredMixin, SuperuserRequiredMixin, DetailView):
     model = RoomCategory
     template_name = 'roomdashboard_detail.html'
+    login_url = 'login'
 
-class RoomDashboardUpdateView(UpdateView):
+class RoomDashboardUpdateView(LoginRequiredMixin, SuperuserRequiredMixin, UpdateView):
     model = RoomCategory
     fields = ('category_name', 'summary', 'room_image', 'price', 'total_rooms')
     template_name = 'roomdashboard_edit.html'
+    login_url = 'login'
 
-class RoomDashboardDeleteView(DeleteView):
+class RoomDashboardDeleteView(LoginRequiredMixin, SuperuserRequiredMixin, DeleteView):
     model = RoomCategory
     template_name = 'roomdashboard_delete.html'
     success_url = reverse_lazy('roomdashboard_list')
+    login_url = 'login'
 
-class RoomDashboardCreateView(CreateView):
+class RoomDashboardCreateView(LoginRequiredMixin, SuperuserRequiredMixin, CreateView):
     model = RoomCategory
     fields = ('category_name', 'summary', 'room_image', 'price', 'total_rooms')
     template_name = 'roomdashboard_new.html'
     success_url = reverse_lazy('roomdashboard_list')
+    login_url = 'login'
