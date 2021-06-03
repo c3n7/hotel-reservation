6c38592515f9c69bff3baec8b61e008aa98302c5
Timothy Karani <kartimothy@gmail.com>
2021-06-03 11:51:10 +0300

    Using sqlite3

--- hotel_reservation_project/settings.py
+++ hotel_reservation_project/settings.py
@@ -83,17 +83,13 @@ WSGI_APPLICATION = 'hotel_reservation_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'avihamahotel',
-        'USER': 'timo',
-        'PASSWORD': 'qazwsxedc',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + "/" + 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
